096abd9bfc04fcf0248d5310ccbeab4ae9345310
patricktingson <patrick_tingson@yahoo.com>
2019-06-20 17:16:53 +0800

    Add project files.

---
+++ XamarinShowCase/XamarinShowCase/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamarinShowCase.Models;
+
+namespace XamarinShowCase.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
